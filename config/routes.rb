c12b29c8406a9825c0cdc8d96bbce96e7cb8065c
Alfredo <alfredo.i.urdaneta@gmail.com>
2017-03-16 17:25:44 -0400

    Created pages controller, added root path, installed bootstrap and implemented styles to root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :survey_results
+root 'pages#thanks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
